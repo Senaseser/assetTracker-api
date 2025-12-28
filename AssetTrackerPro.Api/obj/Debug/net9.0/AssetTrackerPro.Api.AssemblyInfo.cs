3088c584e3e9649e6d58d64864428f6508687dcf
Senaseser <seser0202@gmail.com>
2025-12-28 23:52:28 +0300

    updated

--- AssetTrackerPro.Api/obj/Debug/net9.0/AssetTrackerPro.Api.AssemblyInfo.cs
+++ AssetTrackerPro.Api/obj/Debug/net9.0/AssetTrackerPro.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssetTrackerPro.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ec984e84f1699698e6f6951836dfa01d702984")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssetTrackerPro.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssetTrackerPro.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AssetTrackerPro2.Api/README.md
+++
@@ -1,1 +0,0 @@
-# assetTracker-api
